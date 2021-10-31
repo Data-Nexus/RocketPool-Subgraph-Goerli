765827768478f67267302ec1ca1e7927ae213fdc
Data Nexus <derek.m.meyer@gmail.com>
2021-10-31 08:26:07 -0400

    Update networkNodeBalanceMinipoolMetadata.ts
    Reverting to BigDecimal for debugging

--- src/models/networkNodeBalanceMinipoolMetadata.ts
+++ src/models/networkNodeBalanceMinipoolMetadata.ts
@@ -5,13 +5,13 @@ import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
  */
 export class NetworkNodeBalanceMinipoolMetadata {
     totalNodesWithActiveMinipools: BigInt
-    totalAverageFeeForAllActiveMinipools: BigInt
+    totalAverageFeeForAllActiveMinipools: BigDecimal
     totalMinimumEffectiveRPL: BigInt
     totalMaximumEffectiveRPL: BigInt
 
     constructor() {
       this.totalNodesWithActiveMinipools = BigInt.fromI32(0)
-      this.totalAverageFeeForAllActiveMinipools = BigInt.fromI32(0)
+      this.totalAverageFeeForAllActiveMinipools = BigDecimal.fromString('0')
       this.totalMinimumEffectiveRPL = BigInt.fromI32(0)
       this.totalMaximumEffectiveRPL = BigInt.fromI32(0)
     }
